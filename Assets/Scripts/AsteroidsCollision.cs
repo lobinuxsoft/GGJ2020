a3045c55895d2003da1baf9fdba234205aa96b0e
Matias <lobinux@digilearnnials.com>
2020-02-01 04:38:12 -0300

    #26 Maintenance | Recibir daño
    Primera parte completa.

---
+++ Assets/Scripts/AsteroidsCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsCollision : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        DamageReceiver dr = other.GetComponent<DamageReceiver>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (dr)
+            {
+                
+                var pos = collisionEvents[i].intersection;
+                var force = collisionEvents[i].velocity.magnitude * 10;
+                dr.SetDamage(pos, force);
+            }
+            i++;
+        }
+    }
+}
---
+++ Assets/Scripts/DamageReceiver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageReceiver : MonoBehaviour
+{
+    public void SetDamage(Vector3 pos, float force)
+    {
+        Debug.Log($"Collision point: {pos} / Force: {force}");
+    }
+}
