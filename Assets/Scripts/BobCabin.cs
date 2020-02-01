1eed1a1f28ad7a3d32659214b9ccebb75a0dff88
Matias <lobinux@digilearnnials.com>
2020-02-01 19:00:31 -0300

    Algunos arreglos en las escenas interiores de la nave.
    Ahora se puede cambiar entre la vista interna y la externa.

--- Assets/Scripts/BobCabin.cs
+++ Assets/Scripts/BobCabin.cs
@@ -3,9 +3,18 @@
 [RequireComponent(typeof(BoxCollider))]
 public class BobCabin : MonoBehaviour
 {
+    [SerializeField] GameState gameState;
+    [SerializeField] Transform resetPos;
     void Start()
     {
         GetComponent<BoxCollider>().isTrigger = true;
         gameObject.tag = Tags.BobCabinTag;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameState.currentEntity = GameState.PlayableEntities.Ship;
+        other.transform.position = resetPos.position;
+        other.transform.rotation = resetPos.rotation;
     }
 }
