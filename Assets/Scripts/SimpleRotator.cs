eb0f833ec5ee01d7bbdcd78d68684463d3f93085
Matias <lobinux@digilearnnials.com>
2020-02-02 01:24:56 -0300

    Creacion del Main Menu

---
+++ Assets/Scripts/SimpleRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotator : MonoBehaviour
+{
+    [SerializeField] float speed = 360;
+    [SerializeField] Vector3 axis = Vector3.up;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.Rotate(axis * speed * Time.deltaTime);
+    }
+}
