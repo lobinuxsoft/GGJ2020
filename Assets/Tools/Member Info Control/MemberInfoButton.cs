71449feca9d55f590bb1d0d3d9508c80ad3717aa
Matias <lobinux@digilearnnials.com>
2020-02-02 09:47:49 -0300

    Agregado botones de creditos

---
+++ Assets/Tools/Member Info Control/MemberInfoButton.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MemberInfoButton : MonoBehaviour
+{
+    public MemberInfo memberInfo;
+
+    [SerializeField] TextMeshProUGUI memberNameText;
+    [SerializeField] TextMeshProUGUI memberRoleText;
+    [SerializeField] UnityEngine.UI.Button portfolioUrlButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        memberNameText.text = memberInfo.memberName;
+        memberRoleText.text = memberInfo.memberRole;
+        portfolioUrlButton.onClick.AddListener(OpenPorfolioURL);
+    }
+
+    public void OpenPorfolioURL()
+    {
+        Application.OpenURL(memberInfo.memberPortfolioURL);
+    }
+}
---
+++ Assets/Tools/Member Info Control/MemberInfoControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemberInfoControl : MonoBehaviour
+{
+    [SerializeField] MemberInfoButton memberInfoButton;
+    [SerializeField] Transform container;
+    [SerializeField] List<MemberInfo> members;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(members.Count > 0)
+        {
+            foreach (var item in members)
+            {
+                memberInfoButton.memberInfo = item;
+                MemberInfoButton mib = Instantiate(memberInfoButton, container) as MemberInfoButton;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public struct MemberInfo
+{
+    public string memberName;
+    public string memberRole;
+    public string memberPortfolioURL;
+}
