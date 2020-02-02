810a9bbb2ca8a49d13ee6b23f5183e86388f0aff
Matias <lobinux@digilearnnials.com>
2020-02-02 00:26:47 -0300

    Ni idea de que nombre mandarle a esto...
    Pero anda

---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    [SerializeField] Vector3 offset;
+    [SerializeField] private Transform targetToFollow;
+
+    private void LateUpdate()
+    {
+        transform.position = targetToFollow.position + offset;
+        transform.LookAt(targetToFollow);
+    }
+}
